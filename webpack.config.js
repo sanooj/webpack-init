e36aaeb36443c0da71301165953188f36b1d7fca
Sanooj <sanooj007@gmail.com>
2019-03-13 01:00:05 +0530

    wepack initial upload

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  }
+};
